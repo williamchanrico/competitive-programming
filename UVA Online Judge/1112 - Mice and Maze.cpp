1e17e12784d7ccc1c881107ee9eaa7fb7af8da76
William Chanrico <williamchanrico@yahoo.co.id>
2017-09-18 07:05:55 +0700

    modified uva 1112

--- UVA Online Judge/1112 - Mice and Maze.cpp
+++ UVA Online Judge/1112 - Mice and Maze.cpp
@@ -21,47 +21,41 @@ int main(){
 		for(int a = 0; a < M; a++){
 			scanf("%d %d %d", &A, &B, &C);
 
-			adjList[A - 1].push_back(make_pair(B - 1, C));
+			adjList[B - 1].push_back(make_pair(A - 1, C));
+		}
+
+		vector<int> dist(N, INF);
+		priority_queue<pii, vector<pii>, greater<pii> > pq;
+
+		dist[E - 1] = 0;
+		pq.push(make_pair(0, E - 1));
+
+		while(!pq.empty()){
+			pii front = pq.top();
+
+			pq.pop();
+
+			int d = front.first;
+			int u = front.second;
+
+			if(d > dist[u]) continue;
+
+			for(int a = 0; a < adjList[u].size(); a++){
+				pair<int, int> v = adjList[u][a];
+
+				if(dist[u] + v.second < dist[v.first]){
+					dist[v.first] = dist[u] + v.second;
+
+					pq.push(make_pair(dist[v.first], v.first));
+				}
+			}
 		}
 
 		int ans = 0;
 
-		for(int start = 0; start < N; start++){
-			if(start == E - 1){
+		for(int a = 0; a < N; a++)
+			if(dist[a] <= timer)
 				ans++;
-
-				continue;
-			}
-
-			vector<int> dist(N, INF);
-			priority_queue<pii, vector<pii>, greater<pii> > pq;
-
-			dist[start] = 0;
-			pq.push(make_pair(0, start));
-
-			while(!pq.empty()){
-				pii front = pq.top();
-
-				pq.pop();
-
-				int d = front.first;
-				int u = front.second;
-
-				if(d > dist[u]) continue;
-
-				for(int a = 0; a < adjList[u].size(); a++){
-					pair<int, int> v = adjList[u][a];
-
-					if(dist[u] + v.second < dist[v.first]){
-						dist[v.first] = dist[u] + v.second;
-
-						pq.push(make_pair(dist[v.first], v.first));
-					}
-				}
-			}
-
-			if(dist[E - 1] <= timer) ans++;
-		}	
 
 		printf("%d\n", ans);
 
