e59bea54bf00e2cb99904476ee0c438c68891e7b
William Chanrico <williamchanrico@yahoo.co.id>
2017-11-09 00:29:17 +0700

    fixed solution 10192 - Vacation.cpp

--- UVA Online Judge/10192 - Vacation.cpp
+++ UVA Online Judge/10192 - Vacation.cpp
@@ -13,19 +13,19 @@ int dp(int x, int y){
 		return memo[x][y];
 	if(inp1[x] == inp2[y])
 		return memo[x][y] = 1 + dp(x - 1, y - 1);
-
+	
 	return memo[x][y] = max(dp(x - 1, y), dp(x, y - 1));
 }
 
 int main(){
 	int TC = 1;
 
-	while(scanf("%s", inp1), inp1[0] != '#'){
-		scanf("%s", inp2);
+	while(fgets(inp1, 110, stdin), inp1[0] != '#'){
+		fgets(inp2, 110, stdin);
 
 		memset(memo, -1, sizeof(memo));
 
 		printf("Case #%d: you can visit at most %d cities.\n", 
-			TC++, dp(strlen(inp1) - 1, strlen(inp2) - 1));
+			TC++, dp(strlen(inp1) - 1, strlen(inp2) - 1) - 1);
 	}
 }
