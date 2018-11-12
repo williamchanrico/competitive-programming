71807f9595446db2eb44ff1357f52bb4aa630081
William Chanrico <williamchanrico@gmail.com>
2018-11-12 18:16:25 +0700

    Add hacker-rank/coin-change.cpp

---
+++ Hacker Rank/coin-change.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int coin[100];
+long long memo[500][3000];
+
+long long dp(int idx, int total){
+    if(total == 0) return 1;
+    if(memo[idx][total] != -1) return memo[idx][total];
+    if(idx < 1 || total < 0) return 0;
+
+    return memo[idx][total] = dp(idx, total - coin[idx]) + dp(idx - 1, total);
+}
+
+int main(){
+    int N, M;
+
+    scanf("%d %d", &N, &M);
+    for(int a = 1; a <= M; a++){
+        scanf("%d", &coin[a]);
+    }
+
+    memset(memo, -1, sizeof(memo));
+    printf("%lld\n", dp(M, N));
+}
