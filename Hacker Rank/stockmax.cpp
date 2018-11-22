95037fe571ea749072ea65b0a6147ad88075d3f9
William Chanrico <williamchanrico@gmail.com>
2018-11-22 09:57:44 +0700

    Add stockmax.cpp

---
+++ Hacker Rank/stockmax.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	int T;
+
+	scanf("%d", &T);
+	while(T--){
+		int N, prices[51000];
+
+		scanf("%d", &N);
+		for(int a = N - 1; a >= 0; a--){
+			scanf("%d", &prices[a]);
+		}
+
+		int maxi = -1;
+		long long ans = 0;
+		for(int a = 0; a < N; a++){
+			maxi = max(maxi, prices[a]);
+			ans += maxi - prices[a];
+		}
+
+		printf("%lld\n", ans);
+	}
+}
